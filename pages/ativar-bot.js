ab15ff319aaad13f2e29decc8441482d1083a7c3
Dantas <danttas96@gmail.com>
2025-07-30 20:15:35 -0300

    Adicionado logo e visual bilionário na home

---
+++ pages/ativar-bot.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AtivarBot() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Ativação de Bot</h1>
+      <p>Página temporária de ativação de bot.</p>
+    </div>
+  );
+}
